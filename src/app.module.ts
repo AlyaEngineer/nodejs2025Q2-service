7fc5aaecd1f956969ed30a6fd07734a67352e0da
Alla Tsaiukova <front.dev.edu@gmail.com>
2025-12-07 03:54:09 +0100

    feat: create Prisma module and Prisma Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,14 @@ import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
 import { FavsModule } from './favs/favs.module';
+import { PrismaModule } from './prisma';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
     UserModule,
     ArtistModule,
     AlbumModule,
